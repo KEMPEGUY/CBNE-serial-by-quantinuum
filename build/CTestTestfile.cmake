7430df485513bce07f0b48c74106ca645b246bf9
KEMPEGUY <kem.peguy@aims-cameroon.org>
2025-06-14 03:40:47 +0100

    Add Quantinuum serial CBNE source code

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/peguy/Documents/Peguy_PhD_Thesis/CODE-CBNE-QUANTINUUM/CBNE
+# Build directory: /home/peguy/Documents/Peguy_PhD_Thesis/CODE-CBNE-QUANTINUUM/CBNE/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
