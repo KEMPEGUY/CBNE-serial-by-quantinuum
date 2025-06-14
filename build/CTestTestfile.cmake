5c20d2aadb0f71f2b0d558fbad70ae4dc7a6ba68
KEMPEGUY <kem.peguy@aims-cameroon.org>
2025-06-14 04:13:21 +0100

    Removed large build files and added build/ to .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peguy/Documents/Peguy_PhD_Thesis/CODE-CBNE-QUANTINUUM/CBNE
-# Build directory: /home/peguy/Documents/Peguy_PhD_Thesis/CODE-CBNE-QUANTINUUM/CBNE/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
